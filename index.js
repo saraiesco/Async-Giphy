045fc4f0d7f57ca636884129e57d75e96f9585ad
ghezaii <ghezai@google.com>
2023-10-05 16:32:53 -0400

    async query

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+//console.log(process.env.API_KEY)
+
+// getImage(query) returns a URL link to the gif
+async function getImage(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    const singleImg = data["data"][0]["url"]
+    console.log(singleImg)
+    return singleImg
+}
+
+getImage('cats');
+
+
